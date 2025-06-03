20233f34c048c4221e61852e0cd3956440c5cdd8
Osama Hussein <osamahusseinn2@gmail.com>
2025-06-02 22:09:53 -0400

    Wrote my first program in C++

---
+++ Hello.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main()
+{
+    // Name
+    printf("Hello, my name is Osama Hussein.\n");
+
+    // Hometown
+    printf("My hometown is Muscat, Oman.\n");
+
+    // Favourite hobby
+    printf("My favourite hobby is playing video games.\n");
+}
