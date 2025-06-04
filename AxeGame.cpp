bd470077b23ec3ba6a66a0a9d54b02a437fdbc53
Osama Hussein <osamahusseinn2@gmail.com>
2025-06-04 15:13:28 -0400

    Learned about loops in C++

--- AxeGame.cpp
+++ AxeGame.cpp
@@ -2,15 +2,13 @@
 
 int main()
 {
-    bool equal{4 == 9}; // false
-    bool notEqual{4 != 9}; // true
-    bool less{4 < 9}; // true
-    bool greater{4 > 9};  // false
-    bool lessEqual{4 <= 9}; // true
-    bool greaterEqual{ 4 >= 9};  // false
-
     int width;
     width = 350;
 
     InitWindow(width, 200, "Osama's Window");
+
+    while (true)
+    {
+
+    }
 }
