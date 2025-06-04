f6a391e6bc4e5f1a324679639438a45f35626ebe
Osama Hussein <osamahusseinn2@gmail.com>
2025-06-04 16:53:31 -0400

    Learned about collision detection

--- AxeGame.cpp
+++ AxeGame.cpp
@@ -13,9 +13,23 @@ int main()
 
     int circle_radius{25};
 
+    // Circle edges
+    int l_circle_x{circle_x - circle_radius};
+    int r_circle_x{circle_x + circle_radius};
+    int u_circle_y{circle_y - circle_radius};
+    int b_circle_y{circle_y + circle_radius};
+
     // Axe coordinates
     int axe_x{400};
     int axe_y{0};
+
+    int axe_length{50};
+
+    // Axe edges
+    int l_axe_x{axe_x};
+    int r_axe_x{axe_x + axe_length};
+    int u_axe_y{axe_y};
+    int b_axe_y{axe_y + axe_length};
 
     int direction{10};
 
@@ -27,18 +41,18 @@ int main()
 
         // Game logic begins
         DrawCircle(circle_x, circle_y, circle_radius, BLUE);
-        DrawRectangle(axe_x, axe_y, 50, 50, RED);
+        DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
 
         // Move the axe
         axe_y += direction;
 
-        if (axe_y > 450 || axe_y < 0)
+        if (axe_y > height || axe_y < 0)
         {
             direction = -direction;
         }
 
         // Subtract circle radius to it to make sure the whole circle is visible in the window boundaries
-        if (IsKeyDown(KEY_D) && circle_x < 350 - circle_radius)
+        if (IsKeyDown(KEY_D) && circle_x < width - circle_radius)
         {
             circle_x += 10; // Move right
         }
