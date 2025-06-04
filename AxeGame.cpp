bd1ef71c172362f2d6e261ecde2c62594baa3ca1
Osama Hussein <osamahusseinn2@gmail.com>
2025-06-04 13:54:38 -0400

    Learned about debugging tools and different ways to initialize variables

--- AxeGame.cpp
+++ AxeGame.cpp
@@ -5,12 +5,11 @@ int main()
     int width;
     width = 350;
 
-    float root_beer;
-    root_beer = 1.99;
-    double cheese_burger;
-    cheese_burger = 5.99;
-    bool shouldHaveLunch;
-    shouldHaveLunch = true;
+    float root_beer = 1.99;
+
+    double cheese_burger{5.99};
+    
+    bool shouldHaveLunch{};
 
     InitWindow(width, 200, "Osama's Window");
 }
