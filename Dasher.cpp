e5d9f4432df53b679e4bffb8127d9ee622dddfb3
Osama Hussein <osamahusseinn2@gmail.com>
2025-06-05 15:13:00 -0400

    Drew a spritesheet on window

--- Dasher.cpp
+++ Dasher.cpp
@@ -10,16 +10,23 @@ int main()
 
     // Acceleration due to gravity (pixels/frame)
     const int gravity{1};
-
-    // Rectangle dimensions
-    const int width{50};
-    const int height{80};
-
-    int posY{windowHeight - height};
+    
     int velocity{0};
 
     bool isInAir{false};
     const int jumpVel{-22};
+
+    Texture2D scarfy = LoadTexture("textures/scarfy.png");
+
+    Rectangle scarfyRec;
+    scarfyRec.width = scarfy.width / 6;
+    scarfyRec.height = scarfy.height;
+    scarfyRec.x = 0;
+    scarfyRec.y = 0;
+
+    Vector2 scarfyPos;
+    scarfyPos.x = windowWidth / 2 - scarfyRec.width / 2;
+    scarfyPos.y = windowHeight - scarfyRec.height;
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -28,7 +35,7 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
-        if (posY >= windowHeight - height)
+        if (scarfyPos.y >= windowHeight - scarfyRec.height)
         {
             // Rectangle is on the ground
             velocity = 0;
@@ -50,13 +57,14 @@ int main()
         }
 
         // Update position
-        posY += velocity;
+        scarfyPos.y += velocity;
 
-        DrawRectangle(windowWidth / 2, posY, width, height, BLUE);
+        DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
 
         // End drawing
         EndDrawing();
     }
 
+    UnloadTexture(scarfy);
     CloseWindow();
 }
