3da35d12224880085a9e55a18cd23dce8ac8f550
Osama Hussein <osamahusseinn2@gmail.com>
2025-06-04 22:01:46 -0400

    Initialized velocity and jumping

--- Dasher.cpp
+++ Dasher.cpp
@@ -8,12 +8,28 @@ int main()
 
     InitWindow(windowWidth, windowHeight, "Dapper Dasher!");
 
+    // Rectangle dimensions
+    const int width{50};
+    const int height{80};
+
+    int posY{windowHeight - height};
+    int velocity{0};
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         // Start drawing
         BeginDrawing();
         ClearBackground(WHITE);
+
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            velocity -= 10;
+        }
+
+        posY += velocity;
+
+        DrawRectangle(windowWidth / 2, posY, width, height, BLUE);
 
         // End drawing
         EndDrawing();
