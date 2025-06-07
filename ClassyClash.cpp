870961510dcf511bdf3c65840fd7d0516cc28d36
Osama Hussein <osamahusseinn2@gmail.com>
2025-06-06 20:35:26 -0400

    Added movement for Classy Clash game

--- ClassyClash.cpp
+++ ClassyClash.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main()
 {
@@ -8,6 +9,9 @@ int main()
     InitWindow(windowWidth, windowHeight, "Classy Clash!");
 
     Texture2D map = LoadTexture("nature_tileset/WorldMap.png");
+    Vector2 mapPos{0.0, 0.0};
+
+     float speed{4.0};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -15,7 +19,18 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
-        Vector2 mapPos{0.0, 0.0};
+        Vector2 direction{};
+        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if (IsKeyDown(KEY_D)) direction.x += 1.0;
+        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if (IsKeyDown(KEY_S)) direction.y += 1.0;
+
+        if(Vector2Length(direction) != 0.0)
+        {
+            // Set map to map position minus direction
+            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+        }
+
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
         EndDrawing();
