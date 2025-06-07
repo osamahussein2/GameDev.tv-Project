bc0177630240258dedb38b96b343ecd8acd34677
Osama Hussein <osamahusseinn2@gmail.com>
2025-06-07 19:52:43 -0400

    Created header file and constructor for Character class

---
+++ Character.cpp
@@ -0,0 +1,60 @@
+#include "Character.h"
+
+Character::Character()
+{
+    width = texture.width / maxFrame;
+    height = texture.height;
+}
+
+void Character::setScreenPos(int winWidth, int winHeight)
+{
+    screenPos = {(float)winWidth / 2.0f - 4.0f * (0.5f * width),
+                 (float)winHeight / 2.0f - 4.0f * (0.5f * height)};
+}
+
+void Character::Tick(float deltaTime)
+{
+    Vector2 direction{};
+    if (IsKeyDown(KEY_A))
+        direction.x -= 1.0;
+    if (IsKeyDown(KEY_D))
+        direction.x += 1.0;
+    if (IsKeyDown(KEY_W))
+        direction.y -= 1.0;
+    if (IsKeyDown(KEY_S))
+        direction.y += 1.0;
+
+    if (Vector2Length(direction) != 0.0)
+    {
+        // Set world position to world position plus direction
+        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
+
+        // Terniary operator for condition checks (if true : else false)
+        direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+
+        texture = run;
+    }
+
+    else
+    {
+        texture = idle;
+    }
+
+    // Update animation frame
+    runningTime += deltaTime;
+    
+    if (runningTime >= updateTime)
+    {
+        frame++;
+        runningTime = 0.f;
+        
+        if (frame > maxFrame) frame = 0;
+    }
+    
+    Rectangle source{frame * width, 0.0f, rightLeft * width, height};
+        
+    Rectangle dest{screenPos.x, screenPos.y, 4.0f * width, 4.0f * height};
+
+    // Draw the character
+    DrawTexturePro(texture, source, dest, Vector2Zero(), 0.0f, WHITE);
+}
---
+++ Character.h
@@ -0,0 +1,34 @@
+#include "raylib.h"
+#include "raymath.h"
+
+class Character
+{
+public:
+    Character();
+
+    Vector2 getWorldPos() { return worldPos; }
+    void setScreenPos(int winWidth, int winHeight);
+    void Tick(float deltaTime);
+
+private:
+    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
+
+    Vector2 screenPos{};
+    Vector2 worldPos{};
+
+    // 1 is for facing right and -1 is for facing left
+    float rightLeft{1.f};
+
+    // Animation variables
+    float runningTime{};
+    int frame{};
+    int maxFrame{6};
+    float updateTime{1.f / 12.f};
+
+    float speed{4.f};
+
+    float width{};
+    float height{};
+};
